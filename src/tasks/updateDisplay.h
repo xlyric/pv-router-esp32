23fd07a2ca41ef6d9cf2e7e24de58507e7c22c38
xlyric <cyril.poissonnier+gitkraken@gmail.com>
2021-05-12 09:53:10 +0200

    Update updateDisplay.h

--- src/tasks/updateDisplay.h
+++ src/tasks/updateDisplay.h
@@ -53,8 +53,8 @@ void updateDisplay(void * parameter){
         drawtext10(60,16, injection_type() );
          display.setTextSize(1);
           display.setTextColor(TFT_WHITE,TFT_BLACK);  display.setTextFont(4);
-          display.setCursor(30, 48, 2);  display.print("Dimmer");
-          display.setCursor(150, 48, 2);  display.print("Puissance");
+          display.setCursor(30, 48, 2);  display.print("Dimmer (%)");
+          display.setCursor(150, 48, 2);  display.print("Puissance (W)");
 
       #endif
 
