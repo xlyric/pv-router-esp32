4f3e73c202ffc45a39bebe8600a5f4cbf7f9a70d
Multinet <nicolas.rebillard@gmail.com>
2022-06-18 01:12:08 +0200

    pagination avec le bouton du haut. formatage gettemp.h

--- src/functions/enphaseFunction.h
+++ src/functions/enphaseFunction.h
@@ -2,27 +2,26 @@
 #define ENPHASE_FUNCTIONS
 
 #include <Arduino.h>
+#include <FS.h>
 #include "../config/config.h"
 #include "../config/enums.h"
 #include "../functions/Mqtt_http_Functions.h"
-
-#include <FS.h>
 #include "SPIFFS.h"
 #include "config/enums.h"
 
-HTTPClient httpenphase;  
+HTTPClient httpenphase;
 
 const char *enphase_conf = "/enphase.json";
-extern Configmodule configmodule; 
+extern Configmodule configmodule;
 
 bool loadenphase(const char *filename, Configmodule &configmodule) {
   // Open file for reading
   File configFile = SPIFFS.open(enphase_conf, "r");
   if (!configFile) {
     Serial.println(F("No Enphase used"));
-    
+
     return false;
-    }
+  }
 
   // Allocate a temporary JsonDocument
   // Don't forget to change the capacity to match your requirements.
@@ -33,87 +32,82 @@ bool loadenphase(const char *filename, Configmodule &configmodule) {
   DeserializationError error = deserializeJson(doc, configFile);
   if (error) {
     Serial.println(F("Failed to read Enphase config"));
-    
+
     return false;
   }
 
-  
   // Copy values from the JsonDocument to the Config
-  
-  strlcpy(configmodule.hostname,                  // <- destination
-          doc["IP_ENPHASE"] | "192.168.0.200", // <- source
-          sizeof(configmodule.hostname));         // <- destination's capacity
+
+  strlcpy(configmodule.hostname,                // <- destination
+          doc["IP_ENPHASE"] | "192.168.0.200",  // <- source
+          sizeof(configmodule.hostname));       // <- destination's capacity
   strlcpy(configmodule.port, doc["PORT_ENPHASE"] | "80",
-          sizeof(configmodule.port));         // <- destination's capacity        
-  strlcpy(configmodule.envoy,                  // <- destination
-          doc["Type"] | "S", // <- source
-          sizeof(configmodule.envoy));         // <- destination's capacity
+          sizeof(configmodule.port));   // <- destination's capacity
+  strlcpy(configmodule.envoy,           // <- destination
+          doc["Type"] | "S",            // <- source
+          sizeof(configmodule.envoy));  // <- destination's capacity
   configmodule.pilote = doc["Pilote"] | true;
 
   configFile.close();
 
-
-Serial.println(" enphase config : " + String(configmodule.hostname));
-Serial.println(" enphase mode : " + String(configmodule.envoy));
-return true;
+  Serial.println(" enphase config : " + String(configmodule.hostname));
+  Serial.println(" enphase mode : " + String(configmodule.envoy));
+  return true;
 }
-
-
 
 void Enphase_get(void) {
+  String url = "/404.html";
+  Serial.println("Enphase_get : envoy type=" + String(configmodule.envoy));
+  if (String(configmodule.envoy) == "R") {
+    url = String(EnvoyR);
+    Serial.print("type R");
+    Serial.print(url);
+  }
+  if (String(configmodule.envoy) == "S") {
+    url = String(EnvoyS);
+    Serial.print("type S");
+    Serial.print(url);
+  }
 
-String url = "/404.html";
-Serial.print(configmodule.envoy );
-if ( String(configmodule.envoy) == "R" ) { url = String(EnvoyR) ; Serial.print("type R" ); Serial.print(url);}
-if ( String(configmodule.envoy) == "S" ) { url = String(EnvoyS) ; Serial.print("type S" ); Serial.print(url);}
+  httpenphase.begin(String(configmodule.hostname), atoi(configmodule.port), url);
+  // int httpResponseCode = httpenphase.GET();
 
-httpenphase.begin(String(configmodule.hostname),atoi(configmodule.port),url);
-//int httpResponseCode = httpenphase.GET();
+  /// workaround because envoy is too slow
+  int httpResponseCode;
+  while ((httpResponseCode = httpenphase.GET()) != 200)
+    ;
 
-/// workaround because envoy is too slow 
-int httpResponseCode;
-while ((httpResponseCode=httpenphase.GET()) != 200);
+  // start connection and send HTTP header
 
-// start connection and send HTTP header
+  Serial.print(" httpcode/ fonction mesure: ");
+  Serial.println(httpResponseCode);
 
-Serial.print(" httpcode/ fonction mesure: ");
-Serial.println(httpResponseCode);
-
-if(httpResponseCode == HTTP_CODE_OK) {
-
+  if (httpResponseCode == HTTP_CODE_OK) {
     String payload = httpenphase.getString();
-    
+
     DynamicJsonDocument doc(2048);
     deserializeJson(doc, payload);
-   
 
-    if ( String(configmodule.envoy) == "R" ) { 
-    gDisplayValues.Fronius_prod= int(doc["wattsNow"]); 
-    gDisplayValues.Fronius_conso = int(doc["wattHoursToday"]) ; 
+    if (String(configmodule.envoy) == "R") {
+      gDisplayValues.Fronius_prod = int(doc["wattsNow"]);
+      gDisplayValues.Fronius_conso = int(doc["wattHoursToday"]);
+    } else {
+      gDisplayValues.Fronius_prod = int(doc["production"][1]["wNow"]);
+      gDisplayValues.Fronius_conso = int(doc["consumption"][1]["wNow"]);
+      gDisplayValues.Fronius_totalconso = int(doc["consumption"][0]["wNow"]);
     }
-    else  { 
-    gDisplayValues.Fronius_prod = int(doc["production"][1]["wNow"]); 
-    gDisplayValues.Fronius_conso = int(doc["consumption"][1]["wNow"]) ;
-    gDisplayValues.Fronius_totalconso = int(doc["consumption"][0]["wNow"]) ;
-    }
-    
+
     String test = doc["consumption"][0];
+  } else {
+    Serial.println("timeout");
+  }
+
+  httpenphase.end();
+
+  // debug
+  Serial.print("prod: " + String(gDisplayValues.Fronius_prod));
+  Serial.print(" conso: " + String(gDisplayValues.Fronius_conso));
+  Serial.println(" total conso: " + String(gDisplayValues.Fronius_totalconso));
 }
-else {
-  Serial.println("timeout");
-}
-
-
-
-httpenphase.end();
-
-//debug
-Serial.print("prod: " + String(gDisplayValues.Fronius_prod));
-Serial.print(" conso: "+ String(gDisplayValues.Fronius_conso) );
-
-}
-
-
-
 
 #endif
--- src/tasks/switchDisplay.h
+++ src/tasks/switchDisplay.h
@@ -2,49 +2,53 @@
 #define TASK_SWITCH_DISPLAY
 
 #include <Arduino.h>
-
 #include "../config/config.h"
 
-//#ifdef  TTGO
-//#include <TFT_eSPI.h>
-//extern TFT_eSPI display;
+#ifdef  TTGO
+  #include <TFT_eSPI.h>
+  extern TFT_eSPI display;
+#endif
 
-#define TFT_PIN 4 
+#define TFT_PIN 4
+
 extern DisplayValues gDisplayValues;
 extern Config config; 
 
 void switchDisplay(void * parameter){
-int timer = millis();
-
-for(;;){
-
-if (digitalRead(TFT_PIN)==HIGH && config.ScreenTime !=0 ) { 
-    if ( millis() > timer + config.ScreenTime*1000 ) {
-      digitalWrite(TFT_PIN,LOW);
+  int timer = millis();
+  for(;;) {
+    if (digitalRead(TFT_PIN)==HIGH && config.ScreenTime !=0 ) { 
+        if ( millis() > timer + config.ScreenTime*1000 ) {
+          digitalWrite(TFT_PIN,LOW);
+        }
     }
-}
 
-
-if (digitalRead(SWITCH)==LOW || gDisplayValues.screenstate == HIGH ){ // if right button is pressed or HTTP call 
-    if (digitalRead(TFT_PIN)==HIGH) {             // and the status flag is LOW
-      gDisplayValues.screenstate = LOW ;      
-      digitalWrite(TFT_PIN,LOW);     // and turn Off the OLED
-      }                           // 
-    else {                        // otherwise...
-      gDisplayValues.screenstate = LOW ;
-      digitalWrite(TFT_PIN,HIGH);      // and turn On  the OLED
-      if (config.ScreenTime !=0 ) {
-        timer = millis();
+    if (digitalRead(SWITCH)==LOW || gDisplayValues.screenstate == HIGH ){ // if right button is pressed or HTTP call 
+        if (digitalRead(TFT_PIN)==HIGH) {             // and the status flag is LOW
+          gDisplayValues.screenstate = LOW ;      
+          digitalWrite(TFT_PIN,LOW);     // and turn Off the OLED
+          }                           // 
+        else {                        // otherwise...      
+          Serial.println("button left/bottom pressed");
+          gDisplayValues.screenstate = LOW ;
+          digitalWrite(TFT_PIN,HIGH);      // and turn On  the OLED
+          if (config.ScreenTime !=0 ) {
+            timer = millis();
+          }
+        }
       }
-    }
-                  // wait a sec for the 
-  }                               // hardware to stabilize
 
+    if ( (digitalRead(BUTTON_LEFT)==LOW) ) {
+        gDisplayValues.page++;
+        if (gDisplayValues.page == NB_PAGES+1) {
+          gDisplayValues.page=1;
+        }
+        Serial.println("button left/up pressed");
+        Serial.print("mode=");
+        Serial.println(gDisplayValues.page);
+      }
 
-  vTaskDelay(500 / portTICK_PERIOD_MS);
-  
-  }
-}
-
-//#endif
+      vTaskDelay(500 / portTICK_PERIOD_MS);  
+  } //for
+} //switchDisplay
 #endif
