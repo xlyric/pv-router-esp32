f4cd9307068c190827e4ec03ddfe4201d58eb9aa
xlyric <cyril.poissonnier+gitkraken@gmail.com>
2023-06-22 09:19:17 +0200

    correction process zombie

--- src/tasks/Serial_task.h
+++ src/tasks/Serial_task.h
@@ -11,10 +11,11 @@ extern Configwifi configwifi;
 void serial_read_task(void * parameter){
   for (;;){
         serial_read();
-   } 
+
    // Sleep for 5 seconds, avant de refaire une analyse
     //vTaskDelay(5000 / portTICK_PERIOD_MS);
     vTaskDelay(pdMS_TO_TICKS(5000));
+  }
 }
 
 #endif
