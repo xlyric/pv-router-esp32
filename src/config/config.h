7f234fc6be931a2831f0dd6ae61bc09084deba4b
xlyric <cyril.poissonnier+gitkraken@gmail.com>
2023-09-05 20:37:16 +0200

    correction bug #27 monfiston

--- src/config/config.h
+++ src/config/config.h
@@ -200,7 +200,7 @@ bool discovery_temp = false;
   //  #define ARDUINO_RUNNING_CORE 1
 #endif
 
-#define RELEASE "Version 20230826"
+#define RELEASE "Version 20230905"
     #ifdef LIGHT_FIRMWARE
         #define VERSION "Light " RELEASE
         #else
