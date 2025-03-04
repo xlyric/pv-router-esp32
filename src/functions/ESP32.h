94d3a8c279016f5b21a6ce9b60a9fab1b92dbe44
Nicolas <nicolas.rebillard@laposte.net>
2025-03-04 09:35:25 +0100

    Ajout de ESP32.H pour recuperer les info ESP en vue d'une config plus automatisée future

--- src/functions/ESP32.h
+++ src/functions/ESP32.h
@@ -1,1 +1,26 @@
+#ifndef ESP32_INFO
+#define ESP32_INFO
 
+String getShortChipModel() {
+  String fullModel = ESP.getChipModel();
+  int spaceIndex = fullModel.indexOf(' ');  // Trouve le premier espace
+  if (spaceIndex != -1) {
+      return fullModel.substring(0, spaceIndex);  // Garde uniquement la première partie
+  }
+  return fullModel;  // Si pas d'espace, retourne tel quel
+}
+
+ESP32Info getESP32Info() {
+  ESP32Info info;
+  info.chipModel = ESP.getChipModel();
+  info.chipModelShort = getShortChipModel();
+  info.chipRevision = ESP.getChipRevision();
+  info.chipCores = ESP.getChipCores();
+  info.boardName = ARDUINO_BOARD;
+  info.chipID = ESP.getEfuseMac();
+  return info;
+}
+
+
+
+#endif
