174c2521a2674a7aeeac0eaf9c5f3e0aa33fc814
Multinet33 <nicolas.rebillard@laposte.net>
2025-03-06 20:22:32 +0100

    Code formatting

--- src/config/traduction.h
+++ src/config/traduction.h
@@ -1,44 +1,29 @@
-
 #if LANG_EN 
-
-#define GRID "Grid" // NOSONAR
-#define INJECTION "Injection" // NOSONAR
-#define STABLE "Stable" // NOSONAR
-
-#define BOOTING "Booting" // NOSONAR
- 
-#define WIFINO "Wifi not configured" // NOSONAR
-#define WIFICONNECT "Connecting to " // NOSONAR
-
-#define OLEDSTART "Start Oled" // NOSONAR
-
-#define SPIFFSNO "SPIFFS Files are missing, please upload them\r\n" // NOSONAR
-#define CONFNO "config Files is missing\r\n" // NOSONAR
-
-#define OLEDPOW "Power (W)" // NOSONAR
-#define OLEDNOSIN "No-Sin" // NOSONAR
-
+  #define GRID "Grid" // NOSONAR
+  #define INJECTION "Injection" // NOSONAR
+  #define STABLE "Stable" // NOSONAR
+  #define BOOTING "Booting" // NOSONAR
+  #define WIFINO "Wifi not configured" // NOSONAR
+  #define WIFICONNECT "Connecting to " // NOSONAR
+  #define OLEDSTART "Start Oled" // NOSONAR
+  #define SPIFFSNO "SPIFFS Files are missing, please upload them\r\n" // NOSONAR
+  #define CONFNO "config Files is missing\r\n" // NOSONAR
+  #define OLEDPOW "Power (W)" // NOSONAR
+  #define OLEDNOSIN "No-Sin" // NOSONAR
 #endif
 
 #if LANG_FR
-
-#define GRID "Reseau" // NOSONAR
-#define INJECTION "Injection" // NOSONAR
-#define STABLE "Stable" // NOSONAR
-
-#define BOOTING "Demarrage" // NOSONAR
-
-#define WIFINO "Wifi non configuré" // NOSONAR
-#define WIFICONNECT "Connexion à  " // NOSONAR
-
-#define OLEDSTART "Démarrage Oled" // NOSONAR
-
-#define SPIFFSNO "Fichier SPIFFS absent, Merci de les uploader\r\n" // NOSONAR
-#define CONFNO "Fichier de configuration absent\r\n" // NOSONAR
-
-#define OLEDPOW "Power (W)" // NOSONAR
-#define OLEDNOSIN "No-Sin" // NOSONAR
-
+  #define GRID "Reseau" // NOSONAR
+  #define INJECTION "Injection" // NOSONAR
+  #define STABLE "Stable" // NOSONAR
+  #define BOOTING "Demarrage" // NOSONAR
+  #define WIFINO "Wifi non configuré" // NOSONAR
+  #define WIFICONNECT "Connexion à  " // NOSONAR
+  #define OLEDSTART "Démarrage Oled" // NOSONAR
+  #define SPIFFSNO "Fichier SPIFFS absent, Merci de les uploader\r\n" // NOSONAR
+  #define CONFNO "Fichier de configuration absent\r\n" // NOSONAR
+  #define OLEDPOW "Power (W)" // NOSONAR
+  #define OLEDNOSIN "No-Sin" // NOSONAR
 #endif
 
 #define NOT_ENPHASE "--> Enphase not present\r\n" // NOSONAR
