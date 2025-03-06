e96196702fb4026978dfd0b6dd1814aae2a82380
Multinet33 <nicolas.rebillard@laposte.net>
2025-03-06 20:12:36 +0100

    Code formating / documentation / clean

--- src/tasks/updateDisplay.h
+++ src/tasks/updateDisplay.h
@@ -1,8 +1,20 @@
 #ifndef TASK_UPDATE_DISPLAY
 #define TASK_UPDATE_DISPLAY
 
+//***********************************
+//************* LIBRAIRIES ESP
+//***********************************
 #include <Arduino.h>
+#ifdef ESP32D1MINI_FIRMWARE
+  #include "SSD1306Wire.h"
+#endif
+#ifdef TTGO
+  #include <TFT_eSPI.h>
+#endif
 
+//***********************************
+//************* PROGRAMME PVROUTER
+//***********************************
 #include "../config/config.h"
 #include "functions/appweb.h"
 #include "functions/drawFunctions.h"
@@ -10,31 +22,35 @@
 #include "functions/froniusFunction.h"
 #include "functions/display.h"
 
+//***********************************
+//************* Variables externes
+//***********************************
 #ifdef ESP32D1MINI_FIRMWARE
-#include "SSD1306Wire.h"
-extern SSD1306Wire display;
+  extern SSD1306Wire display;
 #endif
-
 #ifdef TTGO
-#include <TFT_eSPI.h>
-extern TFT_eSPI display;
+  extern TFT_eSPI display;
 #endif
-
 extern DisplayValues gDisplayValues;
 extern Configmodule configmodule;
-
-#define TFT_PIN 4
-
-/**
- * Metafunction that takes care of drawing all the different
- * parts of the display (or not if it's turned off).
- */
 extern Memory task_mem; 
 
+//***********************************
+//************* Constante
+//***********************************
+#define TFT_PIN 4
+
+//***********************************
+//************* updateDisplay
+//***********************************
 void updateDisplay(void* parameter) {
+  /**
+   * Metafunction that takes care of drawing all the different
+   * parts of the display (or not if it's turned off).
+   */
   for (;;) {
     #ifdef TTGO
-     if ( gDisplayValues.option == 0 && digitalRead(TFT_PIN)==HIGH){
+     if ( gDisplayValues.option == 0 && digitalRead(TFT_PIN)==HIGH) {
       call_display();
       }
     #elif ESP32D1MINI_FIRMWARE
@@ -42,13 +58,11 @@ void updateDisplay(void* parameter) {
       ui.update();
       // Serial.println("Update display after");
     #endif
-      task_mem.task_updateDisplay = uxTaskGetStackHighWaterMark(nullptr);
-      // Sleep for 5 seconds, then update display again!
-      vTaskDelay(pdMS_TO_TICKS(4000));
+    
+    task_mem.task_updateDisplay = uxTaskGetStackHighWaterMark(nullptr);
+    // Sleep for 5 seconds, then update display again!
+    vTaskDelay(pdMS_TO_TICKS(4000));
   } // for
 } // updateDisplay
-
-
-
 
 #endif
